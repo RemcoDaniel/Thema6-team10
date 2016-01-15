68c56b8c0e70234ce9e3a90a061f4d3705008fdf
RemcoDaniel <remco.nijkamp@student.hu.nl>
2016-01-15 13:34:09 +0100

    tempsensor aangepast

--- visual studio/Project1/Project1/TempSensor.cpp
+++ visual studio/Project1/Project1/TempSensor.cpp
@@ -5,11 +5,5 @@
 TempSensor::TempSensor(UART uart) : uart(uart) {}
 
 int TempSensor::getTemp() {
-	uart.sendCommand(0x08, 0x00);
-
-
-	int temp = 0;
-
-	// wachten op antwoord....!!!
-	return temp;
+	return uart.sendCommand(TEMPERATURE_REQ, '\0');
 }
