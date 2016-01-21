5b3832b1ad4e6e0636f075ab78852346eb467893
Jordan Ramirez <jordan.ramirez@student.hu.nl>
2016-01-21 15:29:14 +0100

    Added most recent version of wasmachine server & app

---
+++ RaspberryPi/WasmachineSoftware/wasmachine_server/wasmachine_server_code/WasmachineApp.cpp
@@ -0,0 +1,29 @@
+#include "WasmachineApp.h"
+
+WasmachineApp::WasmachineApp(Broadcaster *b):
+broadcaster{ b }{
+}
+
+void WasmachineApp::onTextMessage(const string & msg, WebSocket * ws){
+	if (msg.compare("STATUS_STOP") == 0){
+		std::cout << "Sending stopped to webapp" << std::endl;
+		ws->sendTextMessage("Stopped");
+	}
+	else if(msg.compare("STATUS_START") == 0){
+		ws->sendTextMessage("Starting");
+	}
+
+	std::cout << "Msg Rec: " << msg << std::endl;
+}
+
+void WasmachineApp::onClose(WebSocket * ws){
+	broadcaster->remove(ws);
+	delete ws;
+}
+
+void WasmachineApp::broadcastMessage(const string & msg){
+	broadcaster->broadcast(msg);
+}
+Broadcaster* WasmachineApp::getBroadcaster(){
+	return broadcaster;
+}
